5624cd6abab46f52c1d19c0f81927436951bc86d
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-09-10 18:55:58 +0800

    adding toggle

--- src/Question.js
+++ src/Question.js
@@ -3,11 +3,14 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ question }) => {
   const { title, info } = question;
   const [showInfo, setShowInfo] = useState(false);
+
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">btn</button>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
       {showInfo && <p>{info}</p>}
     </article>
